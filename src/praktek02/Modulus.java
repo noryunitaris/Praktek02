8fde4e0d24748cdef67444735ab6f4c72b7a8e48
noryunitaris <yunitarisnaida@gmail.com>
2018-04-02 22:23:26 -0700

    Membuat Attribut dan Behavior

--- src/praktek02/Modulus.java
+++ src/praktek02/Modulus.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class Modulus {
+    double X;
+    double Y;
     
+    void cetakdata(){
+        System.out.println("++++++++++++");;
+        System.out.println(   "X : "+X);
+        System.out.println(   "Y : "+Y);
+        System.out.println("++++++++++++");;
+    }
 }
