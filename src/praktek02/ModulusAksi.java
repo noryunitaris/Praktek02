0d12f028a1075c51c72ff7fea501ebd19e7fdb65
noryunitaris <yunitarisnaida@gmail.com>
2018-04-02 22:34:08 -0700

    Mengisi nilai attribut

--- src/praktek02/ModulusAksi.java
+++ src/praktek02/ModulusAksi.java
@@ -3,7 +3,8 @@ package praktek02;
 public class ModulusAksi {
     public static void main(String[] args) {
         Modulus r1 = new Modulus();
-        
+       r1.X = 10;
+       r1.Y = 3;
     }
     
 }
